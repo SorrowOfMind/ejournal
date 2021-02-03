f02fe214a9a12d01d9f15b9ea5e93e3d4559f4ca
SorrowOfMind <sortoflifecraze@gmail.com>
2021-02-03 13:07:15 +0100

    save user to db

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $form = $this->createForm(RegistrationType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setEmail($data['email']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $data['password'])
+            );
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('security/register.html.twig', ['form'=>$form->createView()]);
     }
 }
